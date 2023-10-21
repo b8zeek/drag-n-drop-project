4d7857b3f8c1eae3bc2f9edb23e327c9e48d1869
Basic Mirko <bejzik8@gmail.com>
2023-10-21 21:06:17 +0200

    Added Autobind decorator.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+
+    return adjDescriptor;
+}
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -22,13 +36,14 @@ class ProjectInput {
         this.attach();
     }
 
+    @Autobind
     private submitHandler(event: Event) {
         event.preventDefault();
         console.log(this.titleInputElement.value);
     }
 
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler);
     }
 
     private attach() {
