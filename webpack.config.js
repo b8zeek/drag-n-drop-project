68bcd4bd805fe92d856391c89008bf4b10594973
Mirko Basic <bejzik8@gmail.com>
2023-10-22 04:07:43 +0200

    Updated Webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const path = require('path')
 
 module.exports = {
+    mode: 'development',
     entry: './src/app.ts',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname)
+        }
+    },
     output: {
         filename: 'app.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/'
     },
-    devtool: 'inline-source-map',
     module: {
         rules: [
             {
